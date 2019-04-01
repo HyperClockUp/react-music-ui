82b1856253d6f226ce7d833cd53c9a2b06520984
CFX <924093945@qq.com>
2019-04-01 17:16:53 +0800

    cfx

---
+++ src/components/js/CommonHeader.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import App from "./App";
+
+
+class CommonHeader extends Component {
+    render() {
+        return (
+            <div className="common-header-container">
+                <div className="header-logo">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CommonHeader;
