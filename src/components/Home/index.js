842721397d550b508079b43d55b8e21e99977e78
CFX <924093945@qq.com>
2019-04-03 21:00:35 +0800

    basic routes

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react'
+import './index.css'
+
+class Home extends PureComponent {
+    render() {
+        return (
+            <div className='wall-container'>
+
+            </div>
+        )
+    }
+}
+
+export default Home;
