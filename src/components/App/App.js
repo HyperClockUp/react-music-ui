20c732137954ac870d31892dcc94df84250fb4b2
CFX <924093945@qq.com>
2019-04-03 20:31:23 +0800

    basic routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
-import logo from '../../img/logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import logo from '../../img/logo.svg';
 import './App.css';
-import SearchInput from '../SearchInput/index';
+// import SearchInput from '../SearchInput/index';
 import NavigationBar from '../NavigationBar/index';
 import TestComponent from '../TestComponent/index';
+import Home from '../Home/index'
 
 const tags = [
     {
@@ -21,10 +22,10 @@ const tags = [
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className='app-container'>
                 <Router>
                     <NavigationBar tags={tags}/>
-                    <Route path='/' exact component={TestComponent}/>
+                    <Route path='/' exact component={Home}/>
                     <Route path='/music' component={TestComponent}/>
                     <Route path='/community' component={TestComponent}/>
                 </Router>
